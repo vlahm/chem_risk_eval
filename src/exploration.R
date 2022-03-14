7b0786ac9265ec208060a3955b46c4fa80bf03e1
Mike Vlah <vlahm13@gmail.com>
2022-03-13 23:11:46 -0600

    query_envirofacts is all intact, but still sorting out some workarounds for server-side request bugs

--- src/exploration.R
+++ src/exploration.R
@@ -2,14 +2,13 @@ library(tidyverse)
 library(httr)
 library(glue)
 
+# setwd('earthjustice/chem_risk_eval/')
 
 ## 0 - setup ####
 
 source('src/globals.R')
 
-cas = read_csv('data/general/target_substances.csv')
-
-##  1 [RUN ONCE] - retrieve (and write) more information on each substance ####
+## 1 [RUN ONCE] - retrieve (and write) more information on each substance
 
 # d = get_substance_by_cas(cas$CASRN_nohyphens)
 #
@@ -23,20 +22,45 @@ cas = read_csv('data/general/target_substances.csv')
 #
 # write_csv(cas2, 'data/general/target_substances.csv')
 
+cas = read_csv('data/general/target_substances.csv', col_types=cols())
 
 ## 2 - get everything we can out of envirofacts using REST service ####
-#ICIS-AIR, ECHO?, etc
 
-query_envirofacts(table_names,
-                  column_name=NULL,
-                  operator=NULL,
-                  column_value=NULL,
-                  rows=NULL,
-                  # output_format='CSV',
-                  return_count=FALSE)
+# compile data from ICIS-AIR
+icis_air_chem = tibble()
+for(i in seq_len(nrow(cas))){
+
+    srs_id = cas$SRS_id[i]
+
+    icis_air_chem_ = query_envirofacts(
+        table_names=c('ICIS_DMR_FORM_PARAMETER', 'ICIS_ENF_CONCL_POLLUTANT',
+                      'ICIS_LIMIT', 'REF_POLLUTANT', 'TRI_CHEM_INFO'),
+                      # icis_air_chem = query_envirofacts(table_names=c('ICIS_LIMIT'),
+        filter_column='SRS_ID',
+        filter_operator='=',
+        filter_value=srs_id,
+        join_column='SRS_ID',
+        save_intermediates=TRUE,
+        warn=FALSE)
+
+    #currently exploring in get_ef_tablechunk. see HERE note there
+    aaa = query_ef_table(table_name=c('ICIS_LIMIT'), column_name='SRS_ID', operator='=', column_value=as.character(srs_id), warn=TRUE)
+    aaa = query_ef_table(table_name=c('TRI_CHEM_INFO'), column_name='SRS_ID', operator='=', column_value=as.character(srs_id), warn=TRUE)
+    aaa = query_ef_table(table_name=c('ICIS_ENF_CONCL_POLLUTANT'), column_name='SRS_ID', operator='=', column_value=as.character(srs_id), warn=TRUE)
+    aaa = query_ef_table(table_name=c('REF_POLLUTANT'), column_name='SRS_ID', operator='=', column_value=as.character(srs_id), warn=TRUE)
+    aaa = query_ef_table(table_name=c('ICIS_DMR_FORM_PARAMETER'), column_name='SRS_ID', operator='=', column_value=as.character(srs_id), warn=TRUE)
+
+    #save temporary output in case of error, power failure, etc.
+    saveRDS(icis_air_chem_, paste0('~/temp/icis_air_', srs_id, '.rds'))
+    # assign(paste0('icis_air_', srs_id), icis_air_chem_, pos=.GlobalEnv)
+
+    icis_air_chem = bind_rows(icis_air_chem, icis_air_chem_)
+}
+
+#compile data from ECHO?
 
 
-## 3 -EPA's TRI-DRM comparison portal
+## 3 -EPA's TRI-DRM comparison portal ####
 #(water pollutant loading tool custom search)
 #https://echo.epa.gov/trends/loading-tool/get-data/custom-search/
 
--- src/globals.R
+++ src/globals.R
@@ -28,46 +28,244 @@ get_substance_by_cas = function(casrn_vec,
     return(d)
 }
 
-query_envirofacts_chunk = function(table_names,
-                                   column_name=NULL,
-                                   operator=NULL,
-                                   column_value=NULL,
-                                   rows=NULL,
-                                   return_count=FALSE){
+get_ef_tablechunk = function(table_name,
+                             column_name=NULL,
+                             operator=NULL,
+                             column_value=NULL,
+                             rows=NULL){
 
-    # table_names: char vect; up to 3
-    # operator: =, !=, <, >, BEGINNING, CONTAINING
+    # table_name: char string; the name of an envirofacts table
+    # column_name: char: optional; use to filter by column value (see operator)
+    # operator: =, !=, <, >, BEGINNING, CONTAINING; if filtering, the filter expression's operator (with column_value)
+    # column_value: if filtering, the value on the RHS of the filtering expression
     # rows: 'x:y'; max of 100,000
-    # return_count: if true, returns numeric count
 
     query_base = 'https://data.epa.gov/efservice/'
+    # query_base = 'http://iaspub.epa.gov/enviro/efservice/' #old? still in their example
+
+    table_names_ext = ''
+    if(length(table_name) > 1) stop('table_name max length is 1 (maybe raise an issue with EPA about this?)')
+    # if(length(table_names) > 1){ #deprecated for now
+    #     table_names_ext = paste0('/',
+    #                              paste(table_names[2:length(table_names)],
+    #                                    collapse = '/'))
+    #     table_names = table_names[1]
+    # }
 
     # query = 'https://data.epa.gov/efservice/ICIS_LIMIT/REF_POLLUTANT/SRS_ID/439216/rows/1:5/JSON'
-    query = glue('{qb}{tn}{cn}{op}{cv}{ro}{ct}',
+    # query='https://data.epa.gov/efservice/ICIS_LIMIT/SRS_ID/=/5520/csv' ##***
+    query = glue('{qb}{tn}{cn}{op}{cv}{tnx}{ro}/json',
                  qb=query_base,
-                 tn=paste(table_names, collapse='/'),
+                 tn=paste(table_name, collapse='/'),
                  cn=ifelse(is.null(column_name), '', paste0('/', column_name)),
                  op=ifelse(is.null(operator), '', paste0('/', operator)),
                  cv=ifelse(is.null(column_value), '', paste0('/', column_value)),
-                 ro=ifelse(is.null(rows), '', paste0('/', rows)),
-                 ct=ifelse(return_count, '/count', ''))
+                 tnx=table_names_ext,
+                 ro=ifelse(! is.null(rows), paste0('/rows/', rows), ''))
 
     r = httr::GET(query)
     d = httr::content(r, as='text', encoding='UTF-8')
     d = jsonlite::fromJSON(d)
 
+    # read_csv(d)
+    HERE: see *** above for current test query. the issue is that json requests
+    can cause server-side errors with ICIS_LIMIT and ICIS_DMR_FORM_PARAMETER. csv
+    might be g2g, but the column names come back all mangled, so figure that out.
+    could also potentially just request default xml and convert that to csv here
+
+    if(inherits(d, 'list') && ! inherits(d, 'data.frame')){
+        d = as_tibble(d)
+    }
+
     return(d)
 }
 
+get_response_1char = function(msg,
+                              possible_chars,
+                              subsequent_prompt = FALSE){
+
+    #msg: character. a message that will be used to prompt the user
+    #possible_chars: character vector of acceptable single-character responses
+    #subsequent prompt: not to be set directly. This is handled by
+    #   get_response_1char during recursion.
+
+    if(subsequent_prompt){
+        cat(paste('Please choose one of:',
+                  paste(possible_chars,
+                        collapse = ', '),
+                  '\n> '))
+    } else {
+        cat(msg)
+    }
+
+    ch <- as.character(readLines(con = stdin(), 1))
+
+    if(length(ch) == 1 && ch %in% possible_chars){
+        return(ch)
+    } else {
+        get_response_1char(msg = msg,
+                           possible_chars = possible_chars,
+                           subsequent_prompt = TRUE)
+    }
+}
+
+get_chunksets = function(nrows, maxrows){
+
+    # nrows: the number of rows in an envirofacts table. probably queried via query_ef_rows()
+    # maxrows: the maximum number of rows that can be returned via the envirofacts REST-API
+
+    nchunks = nrows %/% maxrows
+    rem = nrows %% maxrows
+    if(rem > 0) nchunks = nchunks + 1
+
+    chunk_starts = seq(1, nrows, maxrows)
+    chunk_ends = 1:nchunks * maxrows
+    if(rem != 0){
+        chunk_ends[nchunks] = chunk_starts[nchunks] + rem - 1
+    }
+
+    chunk_sets = paste(chunk_starts, chunk_ends,
+                       sep=':')
+
+    return(chunk_sets)
+}
+
+query_ef_rows = function(table_name,
+                         column_name=NULL,
+                         operator=NULL,
+                         column_value=NULL){
+
+    # table_name: char string; the name of an envirofacts table
+    # column_name: char: optional; use to filter by column value (see operator)
+    # operator: =, !=, <, >, BEGINNING, CONTAINING; if filtering, the filter expression's operator (with column_value)
+    # column_value: if filtering, the value on the RHS of the filtering expression
+
+    query_base = 'https://data.epa.gov/efservice/'
+
+    if(length(table_name) > 1) stop('this function is for querying rows of a single table')
+
+    query = glue('{qb}{tn}{cn}{op}{cv}/count/json',
+                 qb=query_base,
+                 tn=paste(table_name, collapse='/'),
+                 cn=ifelse(is.null(column_name), '', paste0('/', column_name)),
+                 op=ifelse(is.null(operator), '', paste0('/', operator)),
+                 cv=ifelse(is.null(column_value), '', paste0('/', column_value)))
+
+    r = httr::GET(query)
+    d = httr::content(r, as='text', encoding='UTF-8')
+    d = jsonlite::fromJSON(d)[[1]]
+
+    return(d)
+}
+
+query_ef_table = function(table_name,
+                          column_name=NULL,
+                          operator=NULL,
+                          column_value=NULL,
+                          warn=TRUE){
+
+    # table_name: char string; the name of an envirofacts table
+    # column_name: char: optional; use to filter by column value (see operator)
+    # operator: =, !=, <, >, BEGINNING, CONTAINING; if filtering, the filter expression's operator (with column_value)
+    # column_value: if filtering, the value on the RHS of the filtering expression
+
+    nrows = query_ef_rows(table_name = table_name,
+                          column_name = column_name,
+                          operator = operator,
+                          column_value = column_value)
+
+    if(warn && nrows > 1e6){
+        resp = get_response_1char(msg = paste('This table has',
+                                              nrows,
+                                              'rows. still load into memory? (y/n) >'),
+                                  c('y', 'n'))
+        if(resp == 'n'){
+            message('aborted query')
+            rtn_abrt = 'user aborted'
+            class(rtn_abrt) = 'ef_err'
+            return(rtn_abrt)
+        }
+    } else {
+        print(paste('This table has', nrows, 'row(s).'))
+    }
+
+    if(nrows == 0){
+        message('Returning empty tibble')
+        return(tibble())
+    }
+
+    chunksets = get_chunksets(nrows=nrows,
+                              maxrows=1e5)
+
+    full_table = tibble()
+    task_start = proc.time()
+    for(i in seq_along(chunksets)){
+
+        print(paste('Retrieving chunk', i, 'of', length(chunksets)))
+
+        chnk = get_ef_tablechunk(table_name=table_name,
+                                 column_name=column_name,
+                                 operator=operator,
+                                 column_value=column_value,
+                                 rows=chunksets[i])
+
+        full_table = bind_rows(full_table, chnk)
+    }
+
+    task_time = unname(round((proc.time() - task_start)[3] / 60, 2))
+    print(paste0('Got table ', table_name, ' (', nrows, ' rows) in ', task_time, ' minutes'))
+
+    return(full_table)
+}
+
+# table_name = c('ICIS_LIMIT'); rows = '1:10'
+# table_names = c('tri_facility', 'tri_reporting_form', 'tri_chem_info'); rows = '1:10'
+# column_name = 'state_abbr'; operator = '='; column_value = 'VA'
+# return_count = FALSE
+# table_name = c('ICIS_LIMIT'); rows = '1:10'
+
+# query_ef_rows('XREF_AIR_FAC_INT_PROG_SUBPART')
+# xx = query_ef_table('ref_sic')
+
 query_envirofacts = function(table_names,
-                             column_name=NULL,
-                             operator=NULL,
-                             column_value=NULL,
-                             rows=NULL,
-                             # output_format='CSV',
-                             return_count=FALSE){
+                             filter_column=NULL,
+                             filter_operator=NULL,
+                             filter_value=NULL,
+                             join_column,
+                             save_intermediates=FALSE,
+                             warn=TRUE){
 
-    length(table_names) %% 3
+    # table_names: char vector of envirofacts table names
+    # filter_column: char: optional; use to filter by column value (see filter_operator). must be shared among all tables in table_names.
+    # filter_operator: =, !=, <, >, BEGINNING, CONTAINING; if filtering, the filter expression's filter_operator (with filter_value)
+    # filter_value: if filtering, the value on the RHS of the filtering expression
+    # join_column: the column by which to join tables in table_names. must be shared by all.
+    # save_intermediates: logical. save each table's results to global env, in case something goes wrong?
 
+    #combines results from several envirofacts tables.
+
+    combined_results = tibble()
+    for(i in seq_along(table_names)){
+
+        print(paste('Working on', table_names[i]))
+
+        table_result = query_ef_table(table_names[i],
+                                      column_name=filter_column,
+                                      operator=filter_operator,
+                                      column_value=filter_value,
+                                      warn=warn)
+
+        if(inherits(table_result, 'ef_err')) return()
+
+        if(save_intermediates){
+            print(paste('saving table', table_names[i], 'to global env'))
+            assign(table_names[i], table_result, pos=.GlobalEnv)
+        }
+
+        combined_results = full_join(combined_result, table_result,
+                                     by = toupper(join_column))
+    }
+
+    return(combined_results)
 }
 
